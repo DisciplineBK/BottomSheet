7a6531a6a04c5c499d53f98f5d12fc33e57683f2
DisciplineBK <sthe1357@gmail.com>
2022-03-18 16:10:04 +0900

    기본 소스 코드 파악 및 주석 추가.

--- App.tsx
+++ App.tsx
@@ -6,9 +6,15 @@ import BottomSheet, { BottomSheetRefProps } from './components/BottomSheet';
 
 export default function App() {
   const ref = useRef<BottomSheetRefProps>(null);
+  console.log ('ref :',ref)
 
+  // @to-Check : useCallback은 왜 최초에 실행이 안되지?
+  // 이 함수가 호출되서 ref 값만 바뀌었는데 왜 BottomSheet가 작동하는거지?
   const onPress = useCallback(() => {
+    console.log ('ref_in_onPress :',ref)
+    // 초기화 값을 null로 했는데 어떻게  {"current": {"isActive": [Function anonymous], "scrollTo": [Function _f]}} 값이 나오는거지?
     const isActive = ref?.current?.isActive();
+    console.log ('isActive :',isActive)
     if (isActive) {
       ref?.current?.scrollTo(0);
     } else {
@@ -32,7 +38,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#111',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -40,7 +46,7 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 25,
     aspectRatio: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'green',
     opacity: 0.6,
   },
 });
--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -7,13 +7,14 @@ import Animated, {
     useAnimatedStyle,
     useSharedValue,
     withSpring,
-    withTiming,
 } from 'react-native-reanimated';
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+console.log('SCREEN_HEIGHT :',SCREEN_HEIGHT)
 
 const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 50;
 
+// 외부에서 React.ReactNode를 받아오기 위함
 type BottomSheetProps = {
     children?: React.ReactNode;
 };
@@ -25,13 +26,15 @@ export type BottomSheetRefProps = {
 
 const BottomSheet = React.forwardRef<BottomSheetRefProps, BottomSheetProps>(
     ({ children }, ref) => {
+        // 애니메이션 효과에 사용하는 변수를 선언합니다. (기본 값은 0)
         const translateY = useSharedValue(0);
         const active = useSharedValue(false);
+        const context = useSharedValue({ y: 0 });
 
         const scrollTo = useCallback((destination: number) => {
             'worklet';
             active.value = destination !== 0;
-
+            // 스프링 효과, 지속 시간을 지정할 수는 없다.
             translateY.value = withSpring(destination, { damping: 50 });
         }, []);
 
@@ -39,20 +42,36 @@ const BottomSheet = React.forwardRef<BottomSheetRefProps, BottomSheetProps>(
             return active.value;
         }, []);
 
+        // 부모 컴포넌트가 자식 컴포넌트의 함수를 사용할 수 있게 한다.
         useImperativeHandle(ref, () => ({ scrollTo, isActive }), [
             scrollTo,
             isActive,
         ]);
 
-        const context = useSharedValue({ y: 0 });
+        // Guesture은 Tap, Pan, LongPress, Fling, Pinch, Rotation, ForceTouch 등이 있다.
         const gesture = Gesture.Pan()
+            // Is called when a gesture transitions to the ACTIVE state.
             .onStart(() => {
+                // 시작할때 위치를 저장.
+                console.log ('onStart _ translateY.value',translateY.value)
                 context.value = { y: translateY.value };
             })
+            // Is called every time a gesture is updated while it is in the ACTIVE state.
             .onUpdate((event) => {
+                // console.log ('onUpdate _ event.translationY',event.translationY)
+                // console.log ('onUpdate _ context.value.y',context.value.y)
+                // event.translationY : y축 만큼 얼마나 이동했는지
+                // context.value.y : 시작 위치
                 translateY.value = event.translationY + context.value.y;
+                // 최대 높이를 넘어가지 않도록 함. Math.max 가장 큰 수 리턴
+                // console.log ('onUpdate _ translateY.value 전',translateY.value)
+                // console.log ('onUpdate _ MAX_TRANSLATE_Y',MAX_TRANSLATE_Y)
                 translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y);
+                // console.log ('onUpdate _ translateY.value 후',translateY.value)
+
             })
+            // Is called when a gesture transitions from the ACTIVE state to the END, FAILED, or CANCELLED state.
+            // If the gesture transitions to the END state, the success argument is set to true otherwise it is set to false.
             .onEnd(() => {
                 if (translateY.value > -SCREEN_HEIGHT / 3) {
                     scrollTo(0);
@@ -61,13 +80,16 @@ const BottomSheet = React.forwardRef<BottomSheetRefProps, BottomSheetProps>(
                 }
             });
 
-        const rBottomSheetStyle = useAnimatedStyle(() => {
+        // 애니메이션 효과를 주는 style 의 경우 useAnimatedStyle Hook 을 이용하여 따로 정의합니다.
+        // 제스처를 할때마다 함수가 실행된다.
+        const animatedStyles = useAnimatedStyle(() => {
             const borderRadius = interpolate(
                 translateY.value,
                 [MAX_TRANSLATE_Y + 50, MAX_TRANSLATE_Y],
                 [25, 5],
-                Extrapolate.CLAMP
+                Extrapolate.CLAMP // 속성  EXTEND , CLAMP,  IDENTITY
             );
+            console.log('borderRadius :', borderRadius) // 숫자 반환
 
             return {
                 borderRadius,
@@ -77,7 +99,8 @@ const BottomSheet = React.forwardRef<BottomSheetRefProps, BottomSheetProps>(
 
         return (
             <GestureDetector gesture={gesture}>
-                <Animated.View style={[styles.bottomSheetContainer, rBottomSheetStyle]}>
+                {/* 애니메이션 효과를 위한 컴포넌트 정의하기 */}
+                <Animated.View style={[styles.bottomSheetContainer, animatedStyles]}>
                     <View style={styles.line} />
                     {children}
                 </Animated.View>
@@ -91,8 +114,8 @@ const styles = StyleSheet.create({
         height: SCREEN_HEIGHT,
         width: '100%',
         backgroundColor: 'white',
-        position: 'absolute',
-        top: SCREEN_HEIGHT,
+        position: 'absolute',   // 기본 값을 relevant. absolute로 설정하면 상위 컴포넌트 위치를 기준으로 배치된다.
+        top: SCREEN_HEIGHT,     // top으로 부터 얼마나 떨어질지를 나태내는 값.
         borderRadius: 25,
     },
     line: {
